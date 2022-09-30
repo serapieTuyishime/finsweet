693eb085c1a5f881cc7b73b832fd5055c9623a2e
serapieTuyishime <tuyiserapie1@gmail.com>
2022-09-30 22:22:43 +0200

    Base components done

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*html"],
+  theme: {
+    extend: {
+      colors: {
+        'primary': '#1D2130',
+        'b': '#1D2130',
+      } 
+    },
+  },
+  plugins: [],
+}
