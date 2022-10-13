ee262f712909e4eaf738f47bd628c8ad1d7f391e
serapieTuyishime <tuyiserapie1@gmail.com>
2022-10-13 12:40:09 +0200

    contact: bemified all pages

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,15 @@ module.exports = {
       colors: {
         'primary': '#1D2130',
         'b': '#1D2130',
-      } 
+        'radialgradient': 'background-image: radial-gradient(farthest-corner at 0 50%, rgb(222, 242, 251), rgb(235, 255, 250), white, white, white)',
+        'radial-gradient-center': 'background-image: radial-gradient(rgb(222, 242, 251), rgb(235, 255, 250), white, white, white)'
+      },
+      fontSize:{
+        '3\.5xl':  ['2rem', {
+          lineHeight: '2.4rem',
+          letterSpacing: '-0.02em',
+        }],
+      },
     },
   },
   plugins: [],
